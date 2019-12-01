877951b02ccab2bfadb808c15b64c5197d2c5bc2
CrowHyc <huangyisi_cd@shishike.com>
2019-12-01 15:24:21 +0800

    [Add] - add graphQL demo.

---
+++ src/main/java/com/crowhyc/graphql/Application.java
@@ -0,0 +1,20 @@
+package com.crowhyc.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * Description: com.crowhyc.graphql
+ * </p>
+ * date：2019/12/1
+ * email:crowhyc@163.com
+ *
+ * @author Dean.Hwang
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/crowhyc/graphql/bookdetails/GraphQLProvider.java
@@ -0,0 +1,64 @@
+package com.crowhyc.graphql.bookdetails;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URL;
+
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
+
+/**
+ * <p>
+ * Description: com.crowhyc.graphql.conf
+ * </p>
+ * date：2019/12/1
+ * email:crowhyc@163.com
+ *
+ * @author Dean.Hwang
+ */
+@Component
+public class GraphQLProvider {
+
+    private GraphQL graphQL;
+    @Autowired
+    GraphQLDataFetcher graphQLDataFetchers;
+    @Bean
+    public GraphQL graphQL() {
+        return graphQL;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        URL url = Resources.getResource("schema.graphqls");
+        String sdl = Resources.toString(url, Charsets.UTF_8);
+        GraphQLSchema graphQLSchema = buildSchema(sdl);
+        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+    }
+
+    private GraphQLSchema buildSchema(String sdl) {
+        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
+        RuntimeWiring runtimeWiring = buildWiring();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        return schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
+    }
+
+    private RuntimeWiring buildWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                            .type(newTypeWiring("Query")
+                                    .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher()))
+                            .type(newTypeWiring("Book")
+                                    .dataFetcher("author", graphQLDataFetchers.getAuthorDataFetcher()))
+                            .build();
+    }
+}
